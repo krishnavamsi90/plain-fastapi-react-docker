826f0ccc81dda3dbe8eb688606300005e8e95749
krishnavamsi90 <37618598+krishnavamsi90@users.noreply.github.com>
2024-07-29 16:20:39 +0530

    l

--- script/start_container.sh
+++ script/start_container.sh
@@ -3,4 +3,8 @@ set -e
 
 cd /home/ubuntu
 
+TOKEN=$(aws ecr get-login-password --region ap-southeast-1)
+docker login --username AWS --password-stdin 209314125202.dkr.ecr.ap-southeast-1.amazonaws.com <<< "$TOKEN"
+
+
 docker compose up -d 
