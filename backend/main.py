fab1a70181ce42f0b21fe091e5c2eea3172ebded
krishnavamsi90 <37618598+krishnavamsi90@users.noreply.github.com>
2024-07-29 13:49:39 +0530

    new

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, World!"}
+    secret_key = os.getenv("SECRET_KEY")
+    region = os.getenv("REGION")
+    return {"message": "Hello, World!", "secret_key": secret_key, "region": region}
